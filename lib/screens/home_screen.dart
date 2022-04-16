dd9fe45194eec7a90499ea614f8dba919a84ab44
lemayzeur <lemayzeur@code9haiti.com>
2022-04-15 21:57:55 -0400

    chapter16

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:kwiz_app/services/secure_storage.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
@@ -68,99 +72,124 @@ class  HomeScreenState extends State<HomeScreen>  with TickerProviderStateMixin 
     }
   ];
 
-@override
-void initState() {
-  super.initState();
-  _tabController = TabController(length: categoryList.length, vsync: this);	
-}
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: categoryList.length, vsync: this);	
+    getCartIDs();
+  }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      bottom: TabBar(
-        controller: _tabController,
-        tabs: categoryList.map<Tab>((el) => Tab(child: Text(el['name'])) ).toList(),
+  // final secureStorage = const FlutterSecureStorage();
+  final SecureStorageService _storageService = SecureStorageService();
+
+  List<int> cart = [];
+
+  void getCartIDs() async {
+    var res = await _storageService.read('cart');
+    if(res != null){
+      setState((){
+        cart = json.decode(res).cast<int>();
+        // cart = json.decode(res).map<int>((e) => e as int).toList();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: categoryList.map<Tab>((el) => Tab(child: Text(el['name'])) ).toList(),
+        ),
       ),
-    ),
-    body: TabBarView(
-      controller: _tabController,
-      children: categoryList.map<Widget>((el) => 
-        SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: el['products'].map<Widget>((pr) => 
-              Container(
-                padding: const EdgeInsets.all(12.0),
-                margin: const EdgeInsets.only(bottom: 10.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Text(pr['name'],
-                        style: const TextStyle(fontSize: 22.0),
-                      )
-                    ),
-                    Container(
-                      height: 120,
-                      child: Image.network(
-                        pr['url'],
-                        loadingBuilder: (context, child, progress){
-                          return progress == null ? child : const CircularProgressIndicator();
-                        },
-                        fit: BoxFit.contain,
-                      )
-                    ),
-                    const Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 5.0),
-                      child: Align(
+      body: TabBarView(
+        controller: _tabController,
+        children: categoryList.map<Widget>((el) => 
+          SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: el['products'].map<Widget>((pr) => 
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  margin: const EdgeInsets.only(bottom: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Align(
                         alignment: Alignment.bottomLeft,
-                        child: Text('${pr['price']} HTG',
-                          style: const TextStyle(color: Colors.orangeAccent, fontWeight: FontWeight.bold),
+                        child: Text(pr['name'],
+                          style: const TextStyle(fontSize: 22.0),
+                        )
+                      ),
+                      Container(
+                        height: 120,
+                        child: Image.network(
+                          pr['url'],
+                          loadingBuilder: (context, child, progress){
+                            return progress == null ? child : const CircularProgressIndicator();
+                          },
+                          fit: BoxFit.contain,
+                        )
+                      ),
+                      const Text(
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 5.0),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Text('${pr['price']} HTG',
+                            style: const TextStyle(color: Colors.orangeAccent, fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            // Ajoute nan panye
-                          },
-                          child: const Icon(Icons.shopping_cart),
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.greenAccent),
-                            foregroundColor: MaterialStateProperty.all<Color>(Colors.black)
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              if(cart.contains(pr['id'])){
+                                setState((){
+                                  cart.removeWhere((id) => id == pr['id']);
+                                });
+                              }else{
+                                setState((){
+                                  cart.add(pr['id']);
+                                });
+                              }
+                              _storageService.write('cart', json.encode(cart));
+                            },
+                            child: const Icon(Icons.shopping_cart),
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(cart.contains(pr['id']) ? Colors.greenAccent : Colors.grey),
+                              foregroundColor: MaterialStateProperty.all<Color>(cart.contains(pr['id']) ? Colors.black : Colors.white)
+                            ),
                           ),
-                        ),
-                        const SizedBox(width: 10.0),
-                        ElevatedButton(
-                          onPressed: () {
-                            // Achte kounya
-                          },
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                          const SizedBox(width: 10.0),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Achte kounya
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                            ),
+                            child: const Text('Achte kounya'),
                           ),
-                          child: const Text('Achte kounya'),
-                        ),
-                      ],
-                    ),
-                  ]
+                        ],
+                      ),
+                    ]
+                  ),
                 ),
-              ),
-            ).toList(),
-          )
-        ),
-      ).toList(),
-    )
-  );
-}
+              ).toList(),
+            )
+          ),
+        ).toList(),
+      )
+    );
+  }
 }
